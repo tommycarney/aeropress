f490732febe4543df6f6a7a389c0d3f5c564a0b2
Thomas Carney <tommycarney@gmail.com>
2017-08-16 15:52:07 +0200

    adds async loading

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,16 @@
 class StaticPagesController < ApplicationController
 
   def index
-    @recipes = Recipe.order(:title)
+    static_pages_parameters.nil? ? @recipes = Recipe.order(:title): @recipes = Recipe.limit(params[:amount])
+    respond_to do |format|
+      format.html {}
+      format.js { }
+    end
+  end
+
+  private
+
+  def static_pages_parameters
+    params.permit(:amount)
   end
 end
