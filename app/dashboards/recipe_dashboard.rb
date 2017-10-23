0e63112b8adcb488d1d92f48bb713d2b497f5dbe
Thomas Carney <tommycarney@gmail.com>
2017-10-23 17:16:34 +0200

    adds title and description to dashboard index view

--- app/dashboards/recipe_dashboard.rb
+++ app/dashboards/recipe_dashboard.rb
@@ -19,6 +19,8 @@ class RecipeDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :title,
+    :description
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
